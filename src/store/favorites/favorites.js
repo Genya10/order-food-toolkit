02aaeccd188b6e60c333c7fa161972537f7d4238
Jenya <113167825+Genya10@users.noreply.github.com>
2023-12-04 16:06:21 +0200

    create favorites.js

---
+++ src/store/favorites/favorites.js
@@ -0,0 +1,19 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState =[]
+
+export const favoritesSlice = createSlice({
+  name: "favorites",
+  initialState,
+  reducers: {
+    toggleFavorites: (state, { payload: recipe }) => {
+      const isExists = state.some((r) => r.id === recipe.id);
+      if (isExists) state = state.filter((r) => r.id !== recipe.id);
+      else {
+        state.push(recipe);
+      }
+    },
+  },
+});
+
+export const {action,reducer} = favoritesSlice;
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { reducer } from "./favorites/favorites";
 
 export const store = configureStore({
-    reducer:{},
+   reducer,
 })
