fa95d54793b0f442f0e37a1d9c2329cf6a73b2e0
Jenya <113167825+Genya10@users.noreply.github.com>
2023-12-05 11:34:49 +0200

    create isExists

--- src/recipe-item/RecipeItem.jsx
+++ src/recipe-item/RecipeItem.jsx
@@ -1,13 +1,19 @@
 import cl from './RecipeItem.module.css';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
+import { actions } from '../store/favorites/favorites';
 
 export const RecipeItem=({recipe})=>{
-    const {favorites} = useSelector(state => state)
-    console.log(favorites)
+    const {favorites} = useSelector(state => state);
+    //console.log(favorites);
+    const dispatch = useDispatch();
+
+    const isExists = favorites.some(r => r.id === recipe.id)
+
   return(
     <div className={cl.recipeItem}>
         <h2>{recipe.name}</h2>
-        <button>Add to favotites</button>
+        <button onClick={()=>dispatch(actions.toggleFavorites(recipe))}>
+          {isExists  ? 'Remove from': 'Add to'} favorites</button>
     </div>
   )
 }
--- src/store/favorites/favorites.js
+++ src/store/favorites/favorites.js
@@ -7,8 +7,14 @@ export const favoritesSlice = createSlice({
   initialState,
   reducers: {
     toggleFavorites: (state, { payload: recipe }) => {
+        console.log('hey')
       const isExists = state.some((r) => r.id === recipe.id);
-      if (isExists) state = state.filter((r) => r.id !== recipe.id);
+      if (isExists) {
+         const index = state.findIndex(item => item.id === recipe.id)
+      if(index !== -1){
+        state.splice(index,1)
+      }
+      }
       else {
         state.push(recipe);
       }
