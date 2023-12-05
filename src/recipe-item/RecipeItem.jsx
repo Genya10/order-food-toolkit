c49f2a6c0c6c5494795f40c45496b142d604a1d6
Jenya <113167825+Genya10@users.noreply.github.com>
2023-12-05 12:16:51 +0200

    create useActions

--- src/recipe-item/RecipeItem.jsx
+++ src/recipe-item/RecipeItem.jsx
@@ -1,18 +1,18 @@
 import cl from './RecipeItem.module.css';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector} from 'react-redux';
 import { actions } from '../store/favorites/favorites';
+import { useActions } from '../hooks/useActions';
 
 export const RecipeItem=({recipe})=>{
     const {favorites} = useSelector(state => state);
     //console.log(favorites);
-    const dispatch = useDispatch();
-
+    const {toggleFavorites} = useActions();
     const isExists = favorites.some(r => r.id === recipe.id)
 
   return(
     <div className={cl.recipeItem}>
         <h2>{recipe.name}</h2>
-        <button onClick={()=>dispatch(actions.toggleFavorites(recipe))}>
+        <button onClick={()=>toggleFavorites(recipe)}>
           {isExists  ? 'Remove from': 'Add to'} favorites</button>
     </div>
   )
